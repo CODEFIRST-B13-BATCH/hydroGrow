86d8bf43183a990d26ddd4e1f093090ecfe2bf43
priyankatapkeer <priyanka.tapkeer2426@gmail.com>
2026-03-24 17:20:28 +0530

    Save navbar changes before resolving conflicts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 // import { SuperAdminDashboardComponent } from "./components/super-admin-dashboard/super-admin-dashboard.component";
  
 
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -10,12 +10,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatMenuModule } from '@angular/material/menu';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { CropsComponent } from '../../components/crops/crops/crops.component';
+import { SuperAdminDashboardComponent } from "../../components/super-admin-dashboard/super-admin-dashboard.component";
+import { MatSelect, MatOption } from "@angular/material/select";
+import { MainDashboardComponent } from '../../../main-dashboard/main-dashboard.component';
 @Component({
   selector: 'app-navbar',
-  imports: [ CommonModule,
-    CropsComponent,
+  imports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
@@ -26,20 +26,34 @@ import { CropsComponent } from '../../components/crops/crops/crops.component';
     MatInputModule,
     RouterModule,
     MatExpansionModule,
-    MatMenuModule
+    MatMenuModule,
+    SuperAdminDashboardComponent,
+    MatSelect,
+    MatOption,
+    MainDashboardComponent
 ],
-  standalone: true,
+
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
 
  showCropsMenu = false;
+ 
+ 
+isCollapsed=true;
 
   constructor(private router: Router) {}
 
-  openCrops() {
-    this.showCropsMenu = !this.showCropsMenu;
-    this.router.navigate(['']);
+ onCropChange(value: string) {
+  if (value === 'health') {
+    this.router.navigate(['/crops-health']);
+  } else if (value === 'management') {
+    this.router.navigate(['/crops-management']);
   }
+}
+toggleSidebar() {
+this.isCollapsed= !this.isCollapsed;
+}
+
 }
